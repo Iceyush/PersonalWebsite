b89edee4d23af56c8ec334ddf8b6c63520b96c44
IceYush <regmi.aayushh@gmail.com>
2025-07-08 22:20:40 -0400

    added about page and fixed home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<router-outlet></router-outlet>`
 })
-export class App {
-  protected title = 'personal-website';
-}
+export class App {}
