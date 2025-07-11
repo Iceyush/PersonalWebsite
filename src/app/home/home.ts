c501d89e10c0eebb7bdd9f26a80948342c8fdba7
IceYush <regmi.aayushh@gmail.com>
2025-07-10 21:24:38 -0400

    Refined home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import Navbar from '../navbar/navbar';
+import { RouterLink } from '@angular/router';
+import { LottieComponent } from 'ngx-lottie';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
-  imports: [ Navbar],
+  imports: [ RouterLink,Navbar, LottieComponent],
 
 })
-export default class Home {}
+export default class Home {
+totoroOptions = {
+  path: 'totoro-walk.json', // now at root of public/
+  autoplay: true,
+  loop: true,
+};
+
+}
